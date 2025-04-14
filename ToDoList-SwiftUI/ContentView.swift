becc8c1565e41d06c25b2009ade289bea63d3879
SeongHuiJung <tjdgml150237@naver.com>
2025-04-14 13:05:17 +0900

    Initial commit

---
+++ ToDoList-SwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ToDoList-SwiftUI
+//
+//  Created by 정성희 on 4/14/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
