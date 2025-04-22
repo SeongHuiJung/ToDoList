46c2a7d2dae399b21c59b5f3c719f302765f76dd
SeongHuiJung <tjdgml150237@naver.com>
2025-04-22 14:47:20 +0900

    [Feat] MVVM 구조 세팅

---
+++ ToDoList-SwiftUI/Model/TodoInfoModel.swift
@@ -0,0 +1,14 @@
+//
+//  TodoInfoModel.swift
+//  ToDoList-SwiftUI
+//
+//  Created by 정성희 on 4/22/25.
+//
+
+import Foundation
+
+struct TodoInfoModel {
+    var title: String
+    var id: Int
+    var isCompleted: Bool
+}
