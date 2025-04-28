918929d1de462139598d375bf7072bcd6ec9c5e8
SeongHuiJung <tjdgml150237@naver.com>
2025-04-28 11:21:46 +0900

    [Comment] 함수 설명 추가

--- ToDoList-SwiftUI/ViewModel/TodoViewModel.swift
+++ ToDoList-SwiftUI/ViewModel/TodoViewModel.swift
@@ -18,8 +18,9 @@ class TodoViewModel: ObservableObject {
         fetchToDoData()
     }
     
-    // 앱 실행시 한번 실행
-    // @Published toDoData 에 SwiftData 를 fetch
+    /// SwiftData 데이터 fetch
+    ///
+    /// 앱 실행시 한번만 실행되며, Published 된 toDoData 에 SwiftData 를 fetch 합니다.
     private func fetchToDoData() {
         do {
             let todoData = try modelContext.fetch(FetchDescriptor<TodoInfoModel>())
@@ -31,14 +32,26 @@ class TodoViewModel: ObservableObject {
         }
     }
     
-    // ToDo 추가 함수
+    /// ToDo 데이터 추가
+    ///
+    /// SwiftData 와 메모리 상의 toDoData 에 TodoInfoModel 데이터를 추가합니다.
+    ///
+    /// - Parameters:
+    ///     - text: ToDo 데이터로 추가할 할 일 내용
     func addToDo(text: String) {
         let newTodo = TodoInfoModel(title: text)
         modelContext.insert(newTodo)
         toDoData.insert(newTodo, at: 0)
     }
     
-    // ToDo 완료/완료해제 함수
+    /// ToDo 데이터 완료 및 완료해제
+    ///
+    /// 메모리 상의 toDoData 의 isCompleted 값을 수정합니다. isCompleted 가 true 이면 false로, false 이면 true 로 변경합니다.
+    ///
+    /// SwiftData는 @Model 객체의 속성을 수정하기만 해도 자동으로 변경을 감지하고 저장하기때문에 modelContext 관련 작업을 해주지 않아도 괜찮습니다.
+    ///
+    /// - Parameters:
+    ///     - id: 값을 변경할 ToDo 데이터의 UUID
     func onOffToDo(id: UUID) {
         let idx = findIndex(list: toDoData, id: id)
         
@@ -46,7 +59,12 @@ class TodoViewModel: ObservableObject {
         return
     }
     
-    // ToDo 삭제 함수
+    /// ToDo 데이터 삭제
+    ///
+    /// SwiftData 와 메모리 상의 toDoData 에서 ToDo 데이터를 삭제합니다.
+    ///
+    /// - Parameters:
+    ///     - id: 삭제할 ToDo 데이터의 UUID
     func deleteToDo(id: UUID) {
         let idx = findIndex(list: toDoData, id: id)
         
@@ -56,7 +74,12 @@ class TodoViewModel: ObservableObject {
         return
     }
     
-    // 원하는 UUID를 가진 index를 찾는 함수
+    /// ToDo 리스트에서 원하는 UUID를 가진 index를 반환
+    ///
+    /// - Parameters:
+    ///     - list: TodoInfoModel 리스트
+    ///     - id: 찾으려고 하는 item 의 UUID
+    /// - Returns: 찾은 item 의 index 반환
     private func findIndex(list: [TodoInfoModel], id: UUID) -> Int {
         guard let idx = list.firstIndex(where: { $0.id == id }) else { return 0 }
         return idx
